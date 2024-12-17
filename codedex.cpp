4e1d9dae61dfbdb1df64e1176d92a5bce8e29cad
Alexandr Gorbunov <roboticist.msu@gmail.com>
2024-12-18 00:33:31 +0700

    Initial commit or update

---
+++ codedex.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    cout << n + m << '\n';
+
+    return 0;
+}
